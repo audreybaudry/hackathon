cc1695112bc1609cfded55b346a6c11bd488987a
AUDREY BAUDRY <baudry.audrey@outlook.fr>
2024-07-03 13:51:51 +0200

    homepage

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,11 +4,17 @@ import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import App from "./App";
+import Homepage from "./pages/Homepage";
 
 const router = createBrowserRouter([
   {
-    path: "/",
     element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Homepage />,
+      },
+    ],
   },
 ]);
 
