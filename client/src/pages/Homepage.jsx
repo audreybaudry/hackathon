096abf2b16a4a80a3edd272734c0b6de06420b98
AUDREY BAUDRY <baudry.audrey@outlook.fr>
2024-07-03 19:15:57 +0200

    small fixes

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -10,9 +10,16 @@ function Homepage() {
           d’inclusivité des femmes dans le domaine de l’IT.{" "}
         </p>
         <br />
-        <p>Vous souhaitez obtenir le label “EGAL-IT” ? </p>
+        <p>
+          Vous souhaitez obtenir le label “EGAL-IT” ? Il vous suffit de répondre
+          à notre quizz sur l'inclusivité de votre entreprise et en quelques
+          questions, vous pourrez savoir où vous vous situez concernant cette
+          thématique comparé à d'autres entreprises de l'IT.
+        </p>
         <br />
-        <p>Participez à notre quizz sur l’inclusivité de votre entreprise.</p>
+        <p>
+          Alors n'hésitez plus et cliquez sur commencer pour démarrer le quizz.
+        </p>
       </div>
       <div className="button-homepage">
         <Link to="/quizz" className="link-homepage">
